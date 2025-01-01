8bbe1a1a458e4118e2f387bfb385990c0c845b3a
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2025-01-02 04:35:52 +0530

    Update FundMe.sol

--- Fund/FundMe.sol
+++ Fund/FundMe.sol
@@ -1,1 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
+error FundMe__NotOwner();
+
+contract FundMe {
+    uint256 public constant MINIMUM_CONTRIBUTION = 0.01 ether;
+    address private immutable i_owner;
+    address[] private s_funders;
+    mapping(address => uint256) private s_addressToAmountFunded;
+
+    bool private locked;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    modifier noReentrant() {
+        require(!locked, "ReentrancyGuard: reentrant call");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value >= MINIMUM_CONTRIBUTION,
+            "Minimum contribution is 0.01 ETH"
+        );
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function withdraw() public onlyOwner noReentrant {
+        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function cheaperWithdraw() public onlyOwner noReentrant {
+        address[] memory funders = s_funders;
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function getAddressToAmountFunded(address fundingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+}
